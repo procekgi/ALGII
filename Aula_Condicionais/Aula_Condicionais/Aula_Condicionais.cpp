5eb5f98d2abd3e9349163ecc1abcb7be807568d4
Procekgi <procekgi@gmail.com>
2018-10-18 08:53:09 -0300

    Aula de programação em c sobre condicionais

---
+++ Aula_Condicionais/Aula_Condicionais/Aula_Condicionais.cpp
@@ -0,0 +1,30 @@
+// Aula_Condicionais.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h";
+#include "stdlib.h";
+
+int main()
+{
+	int a;
+	if (true)
+	{
+		printf("Sempre vai imprimir essa frase");
+
+	}
+
+	if (false)
+	{
+		printf("Nunca será impressa");
+
+	}
+
+	if (a > 3)
+	{
+		printf("a eh maior que 3");
+
+	}
+
+    return 0;
+}
+
